eb7b3d7129c6436113214cb5c1f8a0ca0d8b6492
Sanjeet Soni <sanjeetsonyeye1234@gmail.com>
2026-06-09 17:16:10 +0530

    Data sanitization and schema validation done

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send("User added successfully..!");
   } catch (err) {
-    res.status(400).send("Error saving the user");
+    res.status(400).send("Error "+err.message);
   }
 })
 
@@ -66,11 +66,26 @@ app.delete("/user",async (req,res)=>{
 })
 
 
-app.patch("/user",async (req,res)=>{
-  const userId = req.body.userId;
+app.patch("/user/:userId",async (req,res)=>{
+  const userId = req.params?.userId;
   const data = req.body;
+
+  
   try{
-    const updatedUser = await User.findByIdAndUpdate({_id:userId},data);
+    const ALLOWED_UPDATES =["photoUrl","about","gender","age","skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k)=> ALLOWED_UPDATES.includes(k));
+
+    if(!isUpdateAllowed){
+      throw new Error("Update not allowed");
+    }
+
+    if(data?.skills.length>10){
+      throw new Error("You can have atmost 10 skills");
+    }
+    const updatedUser = await User.findByIdAndUpdate({_id:userId},data,{
+      runValidators:true,
+    });
     res.send("User updated successfully");
   }catch (err) {
     res.status(500).send("Something went wrong..!");
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,71 @@
 const mongoose = require('mongoose');
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   firstName:{
-    type:String
+    type:String,
+    required:true,
+    minLength:3,
+    maxLength:25,
   },
   lastName:{
     type:String
   },
   emailId:{
-    type:String
+    type:String,
+    lowercase:true,
+    required:true,
+    unique:true,
+    trim:true,
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("Invalid email address "+value);
+      }
+    }
   },
   password:{
-    type:String
+    type:String,
+    required:true,
+    minLength:8,
+    maxLength:40,
+    validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error("Your password is not strong "+value);
+      }
+    }
   },
   age:{
-    type:Number
+    type:Number,
+    min:16,
+    max:90,
   },
   gender:{
-    type:String
+    type:String,
+    lowercase:true,
+    validate(value){
+      if(!["male","female","others"].includes(value)){
+        throw new Error("Gender data is not valid");
+      }
+    },
   },
+  photoUrl:{
+    type:String,
+    validate(value){
+      if(!validator.isURL(value)){
+        throw new Error("URL is not valid "+value);
+      }
+    },
+    default:"https://png.pngtree.com/png-clipart/20210915/ourmid/pngtree-user-avatar-placeholder-png-image_3918418.jpg"
+  },
+  about:{
+    type:String,
+    default:"Hello, I'm using DevConnect..!"
+  },
+  skills:{
+    type:[String],
+  }
+},{
+  timestamps:true,
 });
 
 module.exports = mongoose.model("User",userSchema);
